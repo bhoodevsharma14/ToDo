9ee23ff2763d19d89c577a3aef6f6e7d3fb3e3ea
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2021-05-18 18:03:46 +0530

    Added: Features like Adding,Deletion Displaying

--- ToDoProject/ToDoProject/settings.py
+++ ToDoProject/ToDoProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'q^8gf8h$rp!2szfhhkxl&f1ynly6l6qpc(dz6*_3c9%hso4h6)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
